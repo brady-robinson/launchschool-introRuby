85443d28258bf3992a361ea3a7e47a41feef87ac
Brady Robinson <bradyrobinson3@gmail.com>
2018-12-28 14:07:14 -0500

    Add files to variable folder

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+full_name = first_name + " " + last_name
+puts "Hello," + " " + full_name
+10.times do
+  puts full_name
+end
